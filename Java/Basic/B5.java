91f65fe78b8277a5d7cfc238056b2fbfd8e129ff
Igor Shcherbinin <inin097@gmail.com>
2018-03-23 12:00:38 -0400

    Update B5.java

--- Java/Basic/B5.java
+++ Java/Basic/B5.java
@@ -2,6 +2,6 @@ public class B5 {
   public static void main(String[] args) {
   int x = 25;
   int y = 5;
-  System.out.println(x + " x "+ y + " = " + x * y);
+  System.out.println(x + " x "+ y + " = " + (x * y));
  }
 }
