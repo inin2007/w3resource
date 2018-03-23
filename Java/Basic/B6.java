9bd94f7947eecbacf12915995af6241a118663ec
Igor Shcherbinin <inin097@gmail.com>
2018-03-23 12:00:13 -0400

    Create B6.java

---
+++ Java/Basic/B6.java
@@ -0,0 +1,11 @@
+public class B6 {
+  public static void main(String[] args) {
+  int x = 125;
+  int y = 24;
+  System.out.println(x + " + " + y + " = " + (x + y));
+  System.out.println(x + " - " + y + " = " + (x - y));
+  System.out.println(x + " x " + y + " = " + (x * y));
+  System.out.println(x + " / " + y + " = " + (x / y));
+  System.out.println(x + " mod " + y + " = " + (x % y));
+ }
+}
