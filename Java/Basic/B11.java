b06cb04ee27c9e20f020cf89b7f010e23cdaedbc
Igor Shcherbinin <inin097@gmail.com>
2018-06-18 15:28:56 -0400

    Create B11.java

---
+++ Java/Basic/B11.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+  double Radius = 7.5;
+  System.out.println("Perimeter is = "+ Radius * 2 * Math.PI);
+    System.out.println("Area is = "+ Math.PI * Radius * Radius);
+ }
+}
