5f4ab86b88f531e226ed03c2566265ebc18d3e32
Igor Shcherbinin <inin097@gmail.com>
2018-06-19 15:43:48 -0400

    Update B14.java

--- Java/Basic/B14.java
+++ Java/Basic/B14.java
@@ -1,14 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("**********===================");
-    System.out.println("********* ===================");
-    System.out.println("**********===================");
-    System.out.println("********* ===================");
-    System.out.println("**********===================");
-    System.out.println("=============================");
-    System.out.println("=============================");
-    System.out.println("=============================");
-    System.out.println("=============================");
-    System.out.println("=============================");
- }
+    String x = "**********====================\n********* ====================";
+    String y = "==============================";
+        for (int i=0; i < 3; i++){
+    System.out.println(x);
+    }
+        for (int i=0; i < 5; i++){
+    System.out.println(y);
+    }
+  }
 }
