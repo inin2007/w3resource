e5b1724ac5cdf41eafa9197a20e1f90011f9c816
Igor Shcherbinin <inin097@gmail.com>
2018-06-19 15:25:22 -0400

    Create B13.java

---
+++ Java/Basic/B13.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+  double Width = 5.5;
+  double Height = 8.5;
+    System.out.println("Area is "+Width+" * "+Height+" = "+Width*Height);
+    System.out.println("Area is 2 * "+Width+" + "+Height+" = "+2*(Width+Height));
+ }
+}
