5f9fee34e41d85862439097b6b9c5b89c967c972
Igor Shcherbinin <inin097@gmail.com>
2018-07-23 14:01:33 -0400

    Create B21.java

---
+++ Java/Basic/B21.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[])
+    {
+        int dec_num, quot, i=1, j;
+        int oct_num[] = new int[100];
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Input a Decimal Number: ");
+        dec_num = scan.nextInt();
+
+        quot = dec_num;
+
+        while(quot != 0)
+        {
+            oct_num[i++] = quot%8;
+            quot = quot/8;
+        }
+
+        System.out.print("Octal number is: ");
+        for(j=i-1; j>0; j--)
+        {
+            System.out.print(oct_num[j]);
+        }
+        System.out.print("\n");
+    }
+}
