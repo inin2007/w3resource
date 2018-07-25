057bfd01e508282c72626a2be8d74bf4915af3eb
Igor Shcherbinin <inin097@gmail.com>
2018-07-25 14:09:51 -0400

    Create B47.java

---
+++ Java/Basic/B47.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class Main {
+
+        public static void main(String args[]) {
+            SimpleDateFormat cdt = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+            cdt.setCalendar(Calendar.getInstance(TimeZone.getTimeZone("GMT")));
+
+            System.out.println("\nNow: "+cdt.format(System.currentTimeMillis()));
+        }
+    }
