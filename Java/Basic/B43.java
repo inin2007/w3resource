0462b533353d15c436c748bb3edb04f473d8d290
Igor Shcherbinin <inin097@gmail.com>
2018-07-25 12:24:17 -0400

    Create B43.java

---
+++ Java/Basic/B43.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            System.out.println("\nTwinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!\n\n");
+        }
+    }
