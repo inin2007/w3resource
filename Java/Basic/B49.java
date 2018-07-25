688d4060d11e6ac624126923c842e95a2980981a
Igor Shcherbinin <inin097@gmail.com>
2018-07-25 14:17:50 -0400

    Create B49.java

---
+++ Java/Basic/B49.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
+            System.out.print("Input a number: ");
+            int n = in.nextInt();
+            if (n % 2 == 0) {
+                System.out.println(1);
+            }
+            else {
+                System.out.println(0);
+            }
+        }
+    }
