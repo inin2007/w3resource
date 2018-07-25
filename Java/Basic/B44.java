6fe77f1a2c81ab372f2252fbfc6c04e2b5902ae5
Igor Shcherbinin <inin097@gmail.com>
2018-07-25 12:36:06 -0400

    Create B44.java

---
+++ Java/Basic/B44.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            int n;
+            Scanner in = new Scanner(System.in);
+            System.out.print("Input number: ");
+            n = in .nextInt();
+            System.out.printf("%d + %d%d  + %d%d%d\n", n, n, n, n, n, n);
+        }
+    }
