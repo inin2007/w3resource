7619fbbc9e5f96580e86a6a7a6fc2e7514ccab8d
Igor Shcherbinin <inin097@gmail.com>
2018-07-25 11:34:18 -0400

    Create B41.java

---
+++ Java/Basic/B41.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.nio.charset.Charset;
+
+public class Main {
+
+        public static void main(String[] String) {
+            int chr = 'Z';
+            System.out.println("The ASCII value of Z is :"+chr);
+        }
+    }
