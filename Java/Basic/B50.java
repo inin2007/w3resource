3982e6ee27853b0a1c7a31b85b4c9e607bca83af
Igor Shcherbinin <inin097@gmail.com>
2018-07-25 15:17:40 -0400

    Create B50.java

---
+++ Java/Basic/B50.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String args[]) {
+            System.out.println("\nDivided by 3: ");
+            for (int i=1; i<100; i++) {
+                if (i%3==0)
+                    System.out.print(i +", ");
+            }
+
+            System.out.println("\n\nDivided by 5: ");
+            for (int i=1; i<100; i++) {
+                if (i%5==0) System.out.print(i +", ");
+            }
+
+            System.out.println("\n\nDivided by 3 & 5: ");
+            for (int i=1; i<100; i++) {
+                if (i%3==0 && i%5==0) System.out.print(i +", ");
+            }
+            System.out.println("\n");
+        }
+    }
