0976a134aae17a0024a0a7299baf758db84dbeb2
Igor Shcherbinin <inin097@gmail.com>
2018-07-26 16:00:33 -0400

    Create B56.java

---
+++ Java/Basic/B56.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args){
+            int x = 5;
+            int y = 20;
+            int p = 3;
+            System.out.println(result(x,y,p));
+        }
+        public static int result(int x, int y, int p) {
+            if (x%p == 0)
+                return( y/p - x/p + 1);
+            return(y/p - x/p);
+        }
+    }
