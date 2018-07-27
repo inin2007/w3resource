3d28b040b35a909c9b683a103f929bfca6abc8a7
Igor Shcherbinin <inin097@gmail.com>
2018-07-27 15:09:42 -0400

    Create B68.java

---
+++ Java/Basic/B68.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            String main_string = "Python 3.0";
+
+            String last_three_chars = main_string.substring(main_string.length() - 3);
+            System.out.println(last_three_chars + last_three_chars + last_three_chars + last_three_chars);
+        }
+    }
