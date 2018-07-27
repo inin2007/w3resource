371b18609fbbeb8121a52f1b1c4e8afe7702ab65
Igor Shcherbinin <inin097@gmail.com>
2018-07-27 13:50:16 -0400

    Create B61.java

---
+++ Java/Basic/B61.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
+            System.out.print("\nInput a word: ");
+            String word = in.nextLine();
+            word = word.trim();
+            String result = "";
+            char[] ch=word.toCharArray();
+            for (int i = ch.length - 1; i >= 0; i--) {
+                result += ch[i];
+            }
+            System.out.println("Reverse word: "+result.trim());
+        }
+    }
