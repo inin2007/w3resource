246748aec0f528f74ce6bc5e8e6988cf1ba3876e
Igor Shcherbinin <inin097@gmail.com>
2018-07-27 14:03:26 -0400

    Create B62.java

---
+++ Java/Basic/B62.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Input the first number : ");
+            int x = in.nextInt();
+            System.out.print("Input the second number: ");
+            int y = in.nextInt();
+            System.out.print("Input the third number : ");
+            int z = in.nextInt();
+            System.out.println((Math.abs(x - y) >= 20 || Math.abs(y - z) >= 20 || Math.abs(z - x) >= 20));
+        }
+    }
