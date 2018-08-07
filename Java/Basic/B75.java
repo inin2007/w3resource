89c31346316b04a6be4860aed487338ef05a6ab3
Igor Shcherbinin <inin097@gmail.com>
2018-08-07 16:46:43 -0400

    Create B75.java

---
+++ Java/Basic/B75.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            int[] num_array = {50, -20, 0, 30, 40, 60, 0};
+
+            System.out.println (num_array.length >= 1 && num_array[0] ==  num_array[num_array.length-1]);
+        }
+    }
