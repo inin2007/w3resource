7acf004230f7523cfa9dd3ce7cf8d5d745b14dcc
Igor Shcherbinin <inin097@gmail.com>
2018-08-07 16:42:14 -0400

    Create B74.java

---
+++ Java/Basic/B74.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            int[] num_array = {10, -20, 0, 30, 40, 60, 10};
+            System.out.println((num_array[0] == 10 || num_array[num_array.length-1] == 10));
+        }
+    }
