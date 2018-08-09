56474bac320b32ef2f958ffd2d2047f199511517
Igor Shcherbinin <inin097@gmail.com>
2018-08-09 15:13:30 -0400

    Create B81.java

---
+++ Java/Basic/B81.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            int[] array_nums = {20, 30, 40};
+            System.out.println("Original Array: "+Arrays.toString(array_nums));
+            int x = array_nums[0];
+            array_nums[0] = array_nums[array_nums.length-1];
+            array_nums[array_nums.length-1] = x;
+            System.out.println("New array after swaping the first and last elements: "+Arrays.toString(array_nums));
+        }
+    }
