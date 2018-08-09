81f055d37b75dca38235d3d3ae079c5a15259644
Igor Shcherbinin <inin097@gmail.com>
2018-08-09 15:02:57 -0400

    Create B78.java

---
+++ Java/Basic/B78.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            int[] array_nums = {5, 7};
+            System.out.println("Original Array: "+Arrays.toString(array_nums));
+            if(array_nums[0] == 4 || array_nums[0] == 7)
+                System.out.println("True");
+            else
+                System.out.println(array_nums[1] == 4 || array_nums[1] == 7);
+
+        }
+    }
