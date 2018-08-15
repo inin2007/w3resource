3f93488ef6cc0f350f87826e0e7d4e1e6f79fdb8
Igor Shcherbinin <inin097@gmail.com>
2018-08-15 11:18:33 -0400

    Create B88.java

---
+++ Java/Basic/B88.java
@@ -0,0 +1,12 @@
+package com.company;
+
+    public class Main {
+
+            public static void main(String[] args)
+            {
+                System.out.println("\nCurrent system environment:");
+                System.out.println(System.getenv());
+                System.out.println("\n\nCurrent system properties:");
+                System.out.println(System.getProperties());
+            }
+        }
