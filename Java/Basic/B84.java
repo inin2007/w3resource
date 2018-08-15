e73c2d81ef5989bdbb0c6ff59c9d2d66d175ae80
Igor Shcherbinin <inin097@gmail.com>
2018-08-15 10:05:52 -0400

    Update B84.java

--- Java/Basic/B84.java
+++ Java/Basic/B84.java
@@ -5,12 +5,8 @@ public class Main {
         public static void main(String[] args)
         {
             String string1 = "Python";
-            int slength = 3;
-            if (slength > string1.length()) {
-                slength = string1.length();
-            }
 
-            String subpart = string1.substring(string1.length()-3);
+                String subpart = string1.substring(string1.length()-3);
             System.out.println(subpart + string1 + subpart);
         }
     }
