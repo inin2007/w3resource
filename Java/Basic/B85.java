ce78a21bc9ae62779ee301dba4e9036771abb50b
Igor Shcherbinin <inin097@gmail.com>
2018-08-15 10:12:15 -0400

    Create B85.java

---
+++ Java/Basic/B85.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            String string1 = "Hello how are you?";
+            System.out.println(string1.startsWith("Hello"));
+        }
+    }
