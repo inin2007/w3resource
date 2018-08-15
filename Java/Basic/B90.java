41c7b25202390f13b73d155e794aa29b577aaf31
Igor Shcherbinin <inin097@gmail.com>
2018-08-15 11:35:15 -0400

    Create B90.java

---
+++ Java/Basic/B90.java
@@ -0,0 +1,19 @@
+package com.company;
+
+    public class Main {
+
+            public static void main(String[] args)
+            {
+                // gets the value of the specified environment variable "PATH"
+                System.out.println("\nEnvironment variable PATH: ");
+                System.out.println(System.getenv("PATH"));
+
+                // gets the value of the specified environment variable "TEMP"
+                System.out.println("\nEnvironment variable TEMP: ");
+                System.out.println(System.getenv("TEMP"));
+
+                // gets the value of the specified environment variable "USERNAME"
+                System.out.println("\nEnvironment variable USERNAME: ");
+                System.out.println(System.getenv("USERNAME"));
+            }
+        }
