309e60a98de45bd68ead13ec09aa642737605fee
Igor Shcherbinin <inin097@gmail.com>
2018-08-17 14:42:21 -0400

    Create B116.java

---
+++ Java/Basic/B116.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            for (int i = 1; i <= 100; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.printf("\n%d: fizz buzz",i);
+                } else if (i % 5 == 0) {
+                    System.out.printf("\n%d: buzz",i);
+                } else if (i % 3 == 0) {
+                    System.out.printf("\n%d: fizz",i);
+                }
+            }
+            System.out.printf("\n");
+        }
+    }
