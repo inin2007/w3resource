8f7f28529c18ce1a7de8ac2cc44499e295222466
Igor Shcherbinin <inin097@gmail.com>
2018-08-17 13:16:16 -0400

    Create B108.java

---
+++ Java/Basic/B108.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] arg) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Input a positive integer: ");
+            int n = in.nextInt();
+            if (n>0)
+            {
+                System.out.print("The single digit number is: "+(n == 0 ? 0 : (n % 9 == 0 ? 9 : n % 9)));
+            }
+            System.out.println("\n");
+        }
+    }
