d208c3b16d8d8dc97ccc2c110d12c453646fd6a1
Igor Shcherbinin <inin097@gmail.com>
2018-09-10 14:11:54 -0400

    Create B119.java

---
+++ Java/Basic/B119.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+    int nums[] = {2, 4, 6, 7, 8};
+    int target = 7;
+        int lower = 0;
+        int upper = nums.length - 1;
+        int index = -1;
+        while (lower <= upper) {
+            int mid = (lower + upper) >> 1;
+            if (nums[mid] == target) {
+                index = mid;
+            }
+            if (nums[mid] >= target) {
+                upper = mid - 1;
+            } else {
+                lower = mid + 1;
+            }
+        }
+        System.out.print("Position of "+target +" is "+index);
+    }
+}
