591510bbd384cf7c843c04e37c42a269fc92740d
Igor Shcherbinin <inin097@gmail.com>
2018-09-11 15:18:16 -0400

    Update DT1.java

--- Java/Data Types/DT1.java
+++ Java/Data Types/DT1.java
@@ -1,1 +1,17 @@
+package com.company;
 
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] Strings) {
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Input a degree in Fahrenheit: ");
+            double fahrenheit = input.nextDouble();
+
+            double  celsius =(( 5 *(fahrenheit - 32.0)) / 9.0);
+            System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celsius + " in Celsius");
+        }
+    }
