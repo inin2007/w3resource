5425db80936a1b8398036849691ebaf05d33232f
Igor Shcherbinin <inin097@gmail.com>
2018-09-13 16:36:13 -0400

    Update CS1.java

--- Java/Conditional Statement/CS1.java
+++ Java/Conditional Statement/CS1.java
@@ -1,1 +1,25 @@
+package com.company;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int input = in.nextInt();
+
+        if (input > 0)
+        {
+            System.out.println("Number is positive");
+        }
+        else if (input < 0)
+        {
+            System.out.println("Number is negative");
+        }
+        else
+        {
+            System.out.println("Number is zero");
+        }
+    }
+}
