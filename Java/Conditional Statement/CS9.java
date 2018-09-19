4d9af77abc429b7d80bfe458c31c60f687335de7
Igor Shcherbinin <inin097@gmail.com>
2018-09-19 12:22:52 -0400

    Create CS9.java

---
+++ Java/Conditional Statement/CS9.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args)
+        {
+            Scanner in = new Scanner(System.in);
+
+            System.out.print("Input the year: ");
+            int year = in.nextInt();
+
+            boolean x = (year % 4) == 0;
+            boolean y = (year % 100) != 0;
+            boolean z = ((year % 100 == 0) && (year % 400 == 0));
+
+            if (x && (y || z))
+            {
+                System.out.println(year + " is a leap year");
+            }
+            else
+            {
+                System.out.println(year + " is not a leap year");
+            }
+        }
+    }
